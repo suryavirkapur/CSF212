64f68810141b65240a245eaed0bbae71dff9a2a0
Suryavir Kapur <52126281+suryavirkapur@users.noreply.github.com>
2024-03-12 13:26:05 +0400

    Update 5.sql

--- Lab/5.sql
+++ Lab/5.sql
@@ -1,50 +1,50 @@
--- Salesmen with all information who live in the city where any of the customers live:
+-- 1
 SELECT *
 FROM SALESMAN
 WHERE city IN (SELECT city FROM CUSTOMER);
 
--- Orders with amounts greater than at least one of the orders on September 10th, 2012:
+-- 2
 SELECT *
 FROM ORDERS
 WHERE purch_amt > (SELECT MIN(purch_amt) FROM ORDERS WHERE ord_date = '2012-09-10');
 
--- Orders with an amount smaller than any amount for a customer in London
+-- 3
 SELECT *
 FROM ORDERS
 WHERE purch_amt <
       (SELECT MIN(purch_amt) FROM ORDERS WHERE customer_id IN (SELECT customer_id FROM CUSTOMER WHERE city = 'London'));
 
--- Customers whose grade is higher than every customer in New York:
+-- 4
 SELECT *
 FROM CUSTOMER
 WHERE grade > (SELECT MAX(grade) FROM CUSTOMER WHERE city = 'New York');
 
--- Customers whose grade is not the same as the grade of any customer in London:
+-- 5
 SELECT *
 FROM CUSTOMER
 WHERE grade NOT IN (SELECT grade FROM CUSTOMER WHERE city = 'London');
 
--- Orders issued by the salesman 'Paul Adam':
+-- 6
 SELECT *
 FROM ORDERS
 WHERE salesman_id = (SELECT salesman_id FROM SALESMAN WHERE name = 'Paul Adam');
 
--- Orders for the salesman who belongs to the city London:
+-- 7
 SELECT *
 FROM ORDERS
 WHERE salesman_id IN (SELECT salesman_id FROM SALESMAN WHERE city = 'London');
 
--- Orders with values greater than the average order value for 10th October 2012:
+-- 8
 SELECT *
 FROM ORDERS
 WHERE purch_amt > (SELECT AVG(purch_amt) FROM ORDERS WHERE ord_date = '2012-10-10');
 
--- Commission of all the salesmen servicing customers in Paris:
-SELECT commission
+-- 9
+SELECT name, commission
 FROM SALESMAN
 WHERE salesman_id IN (SELECT salesman_id FROM CUSTOMER WHERE city = 'Paris');
 
--- Customers with orders on October 5, 2012:
+-- 10
 SELECT *
 FROM CUSTOMER
 WHERE customer_id IN (SELECT customer_id FROM ORDERS WHERE ord_date = '2012-10-05');
